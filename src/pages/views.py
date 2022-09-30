638bf92001274ba61fec6711c45490c6b75aa654
delia gt <delissqt@gmail.com>
2022-09-30 16:20:55 -0500

    Added Views and Urls Examples and Display content parameters of Views Funcions

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,9 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home_view(*args, **kwargs):
+
+def home_view(request, *args, **kwargs):
+    print(args)
+    print('-------')
+    print(kwargs)
+    print('------->')
+    print(request)
+    print(request.user)
+
     return HttpResponse("<h1>Hello World</h1>")# string HTML code
 
 
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
     return HttpResponse("<h1>Contact</h1>")
+
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1>About page</h1>")
+
+def profile_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Profile page</h1>")
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view, contact_view # OR from pages import views
+from pages.views import home_view, contact_view, about_view, profile_view # OR from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'), #OR path('', views.home_view, name='home')
     path('home/', home_view, name='home'),
     path('contact/', contact_view, name='conctact'),
+    path('about/', about_view),
+    path('profile/', profile_view),
 ]
