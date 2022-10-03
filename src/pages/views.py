1318f57daf62e1084cfa61ccaa91b84f0ddb2655
delia gt <delissqt@gmail.com>
2022-10-03 16:18:48 -0500

    Using If Conditions in a Template

--- src/pages/views.py
+++ src/pages/views.py
@@ -16,7 +16,8 @@ def about_view(request, *args, **kwargs):
     my_context = {
         "my_text": "This is about us",
         "my_number": 123456789,
-        "my_list": [123, 456, 789],
+        "this_is_true": True,
+        "my_list": [123, 456, 789, 312, 'abc'],
     }
 
     return render(request, "about.html", my_context)
