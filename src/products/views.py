f0cd618ed17125e67516cd7b7cf9bbdf1d80f908
delia gt <delissqt@gmail.com>
2022-10-04 17:36:36 -0500

    Django Model Forms
    Create a Form Usign Models in Order to Create a Product from From in Web Page Working

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 from .models import Product
+
+from .forms import ProductForm
+
+
 # Create your views here.
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
@@ -14,3 +18,16 @@ def product_detail_view(request):
         'object': obj
     }
     return render(request, "product/detail.html", context)
+
+
+def product_form_view(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        form = ProductForm() # rerender the form (clean the form)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'product/product_create.html', context)
