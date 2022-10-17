327db978403dd3a4f3880b9d56a8d6e328d667d2
delia gt <delissqt@gmail.com>
2022-10-17 12:51:12 -0500

    Class Based Views - DetailView
    v1 Working

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.views.generic import (
     CreateView,
@@ -16,3 +16,8 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all() # <blog>/<model_name>_list.html
+
+
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    queryset = Article.objects.all()
