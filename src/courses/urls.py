63177bff602c34d3579daaf41b0a256342aabe8b
delia gt <delissqt@gmail.com>
2022-10-18 11:39:09 -0500

    Raw Detail Class Based View

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -9,4 +9,5 @@ app_name = 'courses'
 urlpatterns = [
     path('', CourseView.as_view(template_name='contact.html'), name='courses-list'),
     # path('', my_function_base_view, name='course-list'),
+    path('<int:id>/', CourseView.as_view(), name="courses-detail"),
 ]
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+
+from .models import Course
+
 # Create your views here.
 
 
 #BASE VIEW CLASS = VIEW
 class CourseView(View):
-    # template_name = "courses/course_detail.html"
-    template_name = "about.html"
+    template_name = "courses/course_detail.html" # Detail View
 
-    # get is the default
-    def get(self, request,  *args, **kwargs):
-        return render(request, self.template_name, {})
+    def get(self, request, id=None, *args, **kwargs):
+        # get is the default
+        context = {}
+
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context['object'] = obj
+        return render(request, self.template_name, context)
 
     # def post => that´s actually how you´d handle a form
     # def post(self, request, *args, **kwargs):
